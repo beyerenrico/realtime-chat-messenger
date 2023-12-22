48f48df6ac5b7689bb7e12286c6fc8c9e75ad28c
beyerenrico <enrico.beyer@synaigy.com>
2023-12-22 22:59:31 +0100

    Change component structure

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,20 +1,41 @@
 type TypographyProps = {
-  type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
   children: React.ReactNode;
 }
 
-const Typography = ({type, children}: TypographyProps) => {
-  return (
-    <>
-      {type === 'h1' && <h1 className='text-4xl font-bold'>{children}</h1>}
-      {type === 'h2' && <h2 className='text-3xl font-bold'>{children}</h2>}
-      {type === 'h3' && <h3 className='text-2xl font-semibold'>{children}</h3>}
-      {type === 'h4' && <h4 className='text-xl font-semibold'>{children}</h4>}
-      {type === 'h5' && <h5 className='text-lg font-medium'>{children}</h5>}
-      {type === 'h6' && <h6 className='text-base font-bold'>{children}</h6>}
-      {type === 'p' && <p className='text-base'>{children}</p>}
-    </>
-  );
+const Heading1 = ({children}: TypographyProps) => {
+  return <h1 className='text-4xl font-bold'>{children}</h1>;
 };
 
-export default Typography;
+const Heading2 = ({children}: TypographyProps) => {
+  return <h2 className='text-3xl font-bold'>{children}</h2>;
+};
+
+const Heading3 = ({children}: TypographyProps) => {
+  return <h3 className='text-2xl font-semibold'>{children}</h3>;
+};
+
+const Heading4 = ({children}: TypographyProps) => {
+  return <h4 className='text-xl font-semibold'>{children}</h4>;
+};
+
+const Heading5 = ({children}: TypographyProps) => {
+  return <h5 className='text-lg font-medium'>{children}</h5>;
+};
+
+const Heading6 = ({children}: TypographyProps) => {
+  return <h6 className='text-base font-bold'>{children}</h6>;
+};
+
+const Paragraph = ({children}: TypographyProps) => {
+  return <p className='text-base'>{children}</p>;
+};
+
+export {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+  Paragraph,
+};
