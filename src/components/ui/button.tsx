db23fa8dcb7d340961fb5612d5993cc60eb4cdfb
beyerenrico <enrico.beyer@synaigy.com>
2023-12-23 01:33:10 +0100

    Remove redundant margin class

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -6,7 +6,7 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center gap-2 justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -62,7 +62,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           {...props}
         >
           {isLoading && (
-            <LucideLoader2 className='w-4 h-4 mr-2 animate-spin'/>
+            <LucideLoader2 className='w-4 h-4 animate-spin'/>
           )}
           {props.children}
         </Comp>
