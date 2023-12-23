2f1e51e985c00c2ca81b37d4313b3b3e1ed68197
beyerenrico <enrico.beyer@synaigy.com>
2023-12-23 01:31:32 +0100

    Replace legacy toaster with sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
+import {Toaster} from 'sonner';
+import {cn} from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,8 +18,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='en' className='h-full'>
+      <body className={cn(inter.className, 'h-full')}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
