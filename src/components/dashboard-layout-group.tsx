14322eb9380706e35907adb542992f64fd034899
beyerenrico <enrico.beyer@synaigy.com>
2023-12-24 03:38:32 +0100

    feat(dashboard): add theme toggle component

--- src/components/dashboard-layout-group.tsx
+++ src/components/dashboard-layout-group.tsx
@@ -2,6 +2,7 @@
 
 import { Session } from 'next-auth';
 import { FC } from 'react';
+import { ModeToggle } from '@/components/theme-toggle';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 import { Paragraph } from '@/components/ui/typography';
 
@@ -13,8 +14,9 @@ type DashboardLayoutGroupProps = {
 const DashboardLayoutGroup: FC<DashboardLayoutGroupProps> = ({ children, session }) => {
   return (
     <div className='h-full overflow-hidden'>
-      <div className='border-b px-6 h-16 flex items-center'>
+      <div className='border-b px-6 h-16 flex items-center justify-between'>
         <span className='font-bold'>Realtime Chat Messenger</span>
+        <ModeToggle />
       </div>
       <ResizablePanelGroup direction='horizontal'>
         <ResizablePanel defaultSize={15}>
