c93c3681085b1316b6c3be7ea4d882ec9bec5c0b
beyerenrico <enrico.beyer@synaigy.com>
2023-12-24 03:18:14 +0100

    refactor(friends): Replace fetch with axios for better error handling

--- src/components/add-friend-button.tsx
+++ src/components/add-friend-button.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
 import { LucidePlus } from 'lucide-react';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { toast } from 'sonner';
 import { z } from 'zod';
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
@@ -16,6 +16,9 @@ type AddFriendButtonProps = {};
 type FormData = z.infer<typeof addFriendValidator>;
 
 const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [showSuccessState, setShowSuccessState] = useState(false);
+
   const form = useForm<FormData>({
     resolver: zodResolver(addFriendValidator),
     defaultValues: {
@@ -25,20 +28,17 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
 
   const AddFriend = async (email: string) => {
     try {
+      setIsLoading(true);
       const validatedEmail = addFriendValidator.parse({ email });
 
-      await fetch('/api/friends/add', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: validatedEmail }),
-      })
-        .then((res) => res.json())
-        .then(() => {
-          toast.success('Friend request sent');
-        });
+      await axios.post('/api/friends/add', {
+        email: validatedEmail
+      });
+
+      setShowSuccessState(true);
     } catch (error) {
+      setShowSuccessState(false);
+
       if (error instanceof z.ZodError) {
         form.setError('email', {
           message: error.message,
@@ -46,9 +46,18 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
         return;
       }
 
+      if (error instanceof AxiosError) {
+        form.setError('email', {
+          message: error.response?.data
+        });
+        return;
+      }
+
       form.setError('email', {
         message: 'Uh! Something went wrong. Please try again later.',
       });
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -58,24 +67,31 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
 
   return (
     <Form {...form}>
-      <form className='max-w-sm w-full space-y-2' onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name='email'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Add friend by email</FormLabel>
-              <FormControl>
-                <Input {...field} type='email' id='email' placeholder='friend@example.com' />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type='submit' className='mb-2'>
-          <LucidePlus size={16} />
-          Add
-        </Button>
+      <form className='grid grid-cols-1 max-w-md w-full space-y-2' onSubmit={form.handleSubmit(onSubmit)}>
+        <div className='flex gap-2'>
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem className='flex-1'>
+                <FormLabel>Add friend by email</FormLabel>
+                <FormControl>
+                  <Input {...field} type='email' id='email' placeholder='friend@example.com' />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit' className='mt-8' isLoading={isLoading}>
+            <LucidePlus size={16} />
+            Add
+          </Button>
+        </div>
+        {showSuccessState && (
+          <p className='text-sm font-medium text-success'>
+            Friend request sent!
+          </p>
+        )}
       </form>
     </Form>
   );
