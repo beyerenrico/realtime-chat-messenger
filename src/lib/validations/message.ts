d1f33f18feff059128dd9fec5aebcdc9dc225d18
beyerenrico <enrico.beyer@synaigy.com>
2023-12-26 13:39:51 +0100

    feat(chat): add character limit to message content

--- src/lib/validations/message.ts
+++ src/lib/validations/message.ts
@@ -4,8 +4,9 @@ export const messageValidator = z.object({
   id: z.string(),
   senderId: z.string(),
   recipientId: z.string(),
-  content: z.string(),
+  content: z.string().max(2000),
   createdAt: z.number(),
+  seen: z.boolean(),
 });
 
 export const messageListValidator = z.array(messageValidator);
